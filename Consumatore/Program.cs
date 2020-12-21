a80b31aeb83b701794cadb005bed261e64752820
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-12-21 11:10:27 +0100

    Add project files.

---
+++ Consumatore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Prodotti;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumatore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prezzo = 0;
+            int quantita = 0;
+            for (int count = 0;  count < 3; count++)
+            {
+                prezzo = gestioneProdotto.InserisciPrezzo(count);
+                quantita = gestioneProdotto.InserisciQuantita(count);
+                gestioneProdotto.StampaVideo(quantita, prezzo, count);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Prodotti/gestioneProdotto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodotti
+{
+    public class gestioneProdotto
+    {
+
+        public static double InserisciPrezzo(int n)
+        {
+            double prezzo;
+            do
+            {
+                Console.Write($"Inserisci Il prezzo dell {n+1}° prodotto: ");
+                prezzo = double.Parse(Console.ReadLine());
+                if (prezzo < 0)
+                    Console.WriteLine("Prezzo non valido.");
+
+            } while (prezzo <=  0);
+
+            return prezzo;
+        }
+
+        public static int InserisciQuantita(int n)
+        {
+            Console.Write($"Inserisci la quantita del {n+1}° prodotto: ");
+            int quantita = int.Parse(Console.ReadLine());
+            return quantita;
+        }
+
+        public static void StampaVideo(int quantita, double prezzo, int n)
+        {
+            double somma;
+            somma = prezzo * quantita;
+            Console.WriteLine($"Il prezzo totale del {n+1}° prodotto è {somma}");
+        }
+    }
+}
